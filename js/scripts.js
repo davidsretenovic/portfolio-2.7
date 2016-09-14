ceb8765caa8771ec40adde563affd3ee9ec15e9a
David S <david.sretenovic@outlook.com>
2016-09-14 10:59:08 +1000

    added cf 2.7 js requirements

--- js/scripts.js
+++ js/scripts.js
@@ -43,13 +43,39 @@ $(document).ready(function(){
   $('#accordion').collapse().height('auto');
 
   // contact form: on submit
+  // $('#input-submit').on('click', function() {
+  // });
+
+  // contact form: message text count
+  $('#input-message').on('keyup', function() {
+    // console.log("keyup happened"); // log keypress
+    var charCount = $('#input-message').val().length; // store keypresses as variable
+    console.log(charCount) // print amount of characters
+    $("#char-count").html(charCount); // display count
+
+    // conditional
+    if(charCount > 50) {
+      $('#char-count').css('color', 'red'); // turns characters red
+      $('#char-count').css('font-weight', '700'); // increases font weight 
+    } else {
+      $('#char-count').css('color', 'black'); // turns characters black
+    };
+  });
+  
+  // 2.7
   $('#input-submit').on('click', function() {
-    var comment = $('#input-message').val();
-    console.log(comment);
-    $('#visible-comment').html(comment);
-    $('#input-message').hide();
-    $('#submit-result').html("Your message has been received");
-    return false;
+    var msgCount = $('#input-message').val().length;
+    if(msgCount == 0 || msgCount == "") {
+      $('#input-message').css('border', '2px solid red');
+    } else {
+      var comment = $('#input-message').val();
+      console.log(comment);
+      $('#visible-comment').html(comment);
+      $('#input-message').hide();
+      $('#submit-result').html("Your message has been received");
+      return false;
+    };
+
   });
 
 }); // end document ready
